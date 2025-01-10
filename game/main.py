7129d0ac8b3f4bbca2a670b1c7b8a8fa8d136c52
Step <ctep0203@mail.ru>
2025-01-10 05:04:03 +0400

    Переделал стрельбу. Теперь можно стрелять просто зажав кнопку и добавил кд на выстрел

--- game/main.py
+++ game/main.py
@@ -15,7 +15,7 @@ enemy3 = EnemyShip(300, 200, 1, enemy_sprites)
 
 # Интерфейс
 HP1 = HP(128, 16)
-# событие выстрела
+# событие выстрела врагов
 ENEMYSHOOTING = pygame.USEREVENT + 1
 pygame.time.set_timer(ENEMYSHOOTING, 1000)
 # событие появления хилки
@@ -25,6 +25,10 @@ pygame.time.set_timer(HPBOOSTSPAWN, 10000)
 SPEEDUP = pygame.USEREVENT + 3
 SPEEDUPCD = pygame.USEREVENT + 4
 speed_boost = False
+# КД на выстрел
+can_shoot = True
+shooting = False
+SHOOTCD = pygame.USEREVENT + 5
 
 running = True
 while running:
@@ -34,13 +38,23 @@ while running:
         player.handle_input(event)
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
-                player.main_ship_shooting()
+                shooting = True
             if event.key == pygame.K_LSHIFT:
                 if not speed_boost:  # проверяем прошло ли кд
                     player.speed += 5  # увеличиваем скорость и запускаем таймера
                     pygame.time.set_timer(SPEEDUP, 3000)
                     pygame.time.set_timer(SPEEDUPCD, 6000)
                     speed_boost = True
+
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_SPACE:
+                shooting = False
+        # Выстрел
+        if shooting and can_shoot:
+            player.main_ship_shooting()
+            pygame.time.set_timer(SHOOTCD, 500)  # запуск кд на выстрел
+            can_shoot = False
+        # События
         if event.type == ENEMYSHOOTING:
             for enemy in enemy_sprites:
                 enemy.enemy_shooting()
@@ -50,6 +64,8 @@ while running:
             player.speed = SHIP_SPEED
         if event.type == SPEEDUPCD:  # прошло кд и можно опять использовать ускорение
             speed_boost = False
+        if event.type == SHOOTCD:
+            can_shoot = True
     # проверка на потерю хп чтобы удалить спрайты
     hp_count = my_font.render(str(player.hp), False, (255, 255, 255))
     all_sprites.update()
